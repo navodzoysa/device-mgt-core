8590e98a4d259414c65351f33146c0ada535875b
NKavindya <110715995+NKavindya@users.noreply.github.com>
2025-11-16 23:54:56 +0530

    Add a null check for tenantManager before invoking publishScopesToTenant

--- components/tenant-mgt/io.entgra.device.mgt.core.tenant.mgt.core/src/main/java/io/entgra/device/mgt/core/tenant/mgt/core/listener/DeviceMgtTenantListener.java
+++ components/tenant-mgt/io.entgra.device.mgt.core.tenant.mgt.core/src/main/java/io/entgra/device/mgt/core/tenant/mgt/core/listener/DeviceMgtTenantListener.java
@@ -64,10 +64,10 @@ public class DeviceMgtTenantListener implements TenantMgtListener {
     @Override
     public void onTenantInitialActivation(int tenantId) throws StratosException {
         TenantManager tenantManager = TenantMgtDataHolder.getInstance().getTenantManager();
-        String tenantDomain = null;
         try {
-            tenantDomain = tenantManager.getTenantDomain(tenantId);
-            tenantManager.publishScopesToTenant(tenantDomain);
+            if (tenantManager != null) {
+                tenantManager.publishScopesToTenant(tenantManager.getTenantDomain(tenantId));
+            }
         } catch (TenantMgtException e) {
             log.error("Error occurred while executing tenant initial activation flow", e);
         }
